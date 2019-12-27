739dcdf444f448ce2db4bef0d92c77079f303cec
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-12-27 17:16:01 -0500

    Commented settings class

--- app/src/main/java/com/linklab/inertia/besic/Settings.java
+++ app/src/main/java/com/linklab/inertia/besic/Settings.java
@@ -1,29 +1,46 @@
 package com.linklab.inertia.besic;
 
-import android.annotation.SuppressLint;
+/*
+ * Imports needed by the system to function appropriately
+ */
+import android.annotation.*;
 import android.os.*;
 import android.preference.*;
 import androidx.annotation.*;
 
-@SuppressLint("ExportedPreferenceActivity")
+/**
+ * This class is responsible for setting up the settings for the application.
+ * This calls on a specified settings xml file that creates the appropriate values and their defaults.
+ */
+@SuppressLint("ExportedPreferenceActivity")     // Suppresses the intent filter added in the manifest file
 public class Settings extends PreferenceActivity
 {
+    /**
+     * This is the classes create method that starts the settings activity
+     * @param savedInstanceState is a parameter passed into the method
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);     // Calls the super class
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
-                new MainSettingFragment()).commit();
+        this.getFragmentManager().beginTransaction().replace(android.R.id.content, new MainSettingFragment()).commit();      // Sets up the settings screen based on the appropriate xml file
     }
 
+    /**
+     * A new class created in the preferences that links to the made settings file
+     */
     public static class MainSettingFragment extends PreferenceFragment
     {
+        /**
+         * This is the classes create method that creates the setting activity
+         * @param savedInstanceState is a parameter passed into the method
+         */
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState)
         {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings);
+            super.onCreate(savedInstanceState);     // Call to the superclass
+            this.addPreferencesFromResource(R.xml.settings);     // Creates the preference resource from the specified xml file
         }
     }
 }
