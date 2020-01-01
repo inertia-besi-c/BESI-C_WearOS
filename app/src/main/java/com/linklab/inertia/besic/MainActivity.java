52f3eeffb05d2c168dec985917e732f410e688ca
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-01-01 14:45:50 -0500

    Commented the main activity class

--- app/src/main/java/com/linklab/inertia/besic/MainActivity.java
+++ app/src/main/java/com/linklab/inertia/besic/MainActivity.java
@@ -1,5 +1,8 @@
 package com.linklab.inertia.besic;
 
+/*
+ * Imports needed by the system to function appropriately
+ */
 import android.content.pm.PackageManager;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
@@ -11,32 +14,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+/**
+ * This is the main process that is fired upon the application being initiated on the device.
+ * This class asks the user to pick what watchface they would like, allows the developer to set
+ * the preferences of the device, and also checks for the required permissions the application needs
+ */
 public class MainActivity extends AppCompatActivity
 {
+    /**
+     * This method is run when the application is called at anytime.
+     * @param savedInstanceState is a parameter passed into the super class
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);     // Creates an instance of the application
 
-        Intent changeWatchFace = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
-                new ComponentName(getPackageName(), WatchFace.class.getName()));
-        startActivity(changeWatchFace);
+        Intent changeWatchFace = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,        // Gets a watchface picker to show
+                new ComponentName(getPackageName(), WatchFace.class.getName()));        // Shows the components of the watchface
+        startActivity(changeWatchFace);     // Starts the intent for the watchface picker
 
-        Intent startSettings = new Intent(MainActivity.this, Settings.class);
-        startActivity(startSettings);
+        Intent startSettings = new Intent(MainActivity.this, Settings.class);       // Starts a new intent for the settings class
+        startActivity(startSettings);       // Starts the intent for the settings
 
-        this.CheckPermissions();
+        this.CheckPermissions();        // Calls the method to check for the required permissions for the device.
 
-        finish();
+        finish();       // Finishes the activity and quits it.
     }
 
+    /**
+     * This method checks for the required permission for the application. If the permissions is not granted by the device,
+     * it prompts the user to grant such permission to the application.
+     */
     private void CheckPermissions()
     {
         String[] Required_Permissions =     // Checks if Device has permission to work on device.
                 {
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,     // This is to access the storage
                         Manifest.permission.READ_EXTERNAL_STORAGE,      // This is to access the storage
-                        Manifest.permission.VIBRATE,
+                        Manifest.permission.VIBRATE,        // This is to access the vibrator of the device
                         Manifest.permission.BODY_SENSORS,       // This is to access the sensors of the device
                         Manifest.permission.ACCESS_WIFI_STATE,      // This is to access the wifi of the device.
                         Manifest.permission.CHANGE_WIFI_STATE,      // This is to change the wifi state of the device.
