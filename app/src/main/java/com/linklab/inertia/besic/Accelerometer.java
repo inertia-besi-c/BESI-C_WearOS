920353266e46f763a3cedb08c51bf350a1dfcf8b
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-01-19 22:52:32 -0500

    Added comments to the class

--- app/src/main/java/com/linklab/inertia/besic/Accelerometer.java
+++ app/src/main/java/com/linklab/inertia/besic/Accelerometer.java
@@ -1,108 +1,146 @@
 package com.linklab.inertia.besic;
 
+/*
+ * Imports needed by the system to function appropriately
+ */
 import android.content.SharedPreferences;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.app.Service;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
 import android.preference.PreferenceManager;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import android.content.Intent;
 
 import java.text.DecimalFormat;
 import java.util.Objects;
 
+/**
+ * This class is responsible for the gathering and logging of the accelerometer data obtained from the wearable device.
+ * This class is designed to run for the duration of the application's cycle with no sort of modulation.
+ */
 public class Accelerometer extends Service implements SensorEventListener
 {
-    private SensorManager sensorManager;
-    private SharedPreferences sharedPreferences;
-    private Sensor accelerometer;
-    private DataLogger dataLogger;
-    private SystemInformation systemInformation;
-    private DecimalFormat decimalFormat;
-    private StringBuilder stringBuilder1, stringBuilder2;
-    private int currentCount;
-    private double[] accelerometer_data;
+    private SensorManager sensorManager;        // Initializes a listener
+    private SharedPreferences sharedPreferences;        // Initializes a shared preference variable
+    private Sensor accelerometer;       // Sets a accelerometer sensor
+    private String linearx, lineary, linearz;       // Sets up the string of the class
+    private DataLogger dataLogger;      // Sets the datalogger variable
+    private SystemInformation systemInformation;        // Sets a system preference variable
+    private DecimalFormat decimalFormat;        // Initializes a decimal formatter
+    private StringBuilder stringBuilder1, stringBuilder2;       // Sets up the string builder variables
+    private int currentCount;       // Sets up the integers of the class
+    private double[] accelerometer_data;        // Sets up a list of doubles
 
+    /**
+     * The constructor for the class.
+     * This is called when the class is run for the first time
+     */
     public Accelerometer()
     {
-        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        this.decimalFormat = new DecimalFormat("#.####");
-        this.systemInformation = new SystemInformation();
-        this.stringBuilder1 = new StringBuilder();
-        this.stringBuilder2 = new StringBuilder();
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());        // Sets up the shared preference
+        this.decimalFormat = new DecimalFormat("#.####");       // Sets up the decimal formatter
+        this.systemInformation = new SystemInformation();       // Sets up the system information
+        this.stringBuilder1 = new StringBuilder();      // Sets up a string builder
+        this.stringBuilder2 = new StringBuilder();      // Sets up a string builder
 
-        this.currentCount = 0;
+        this.currentCount = 0;      // Sets up a variable
     }
 
+    /**
+     * This method is called as soon as the sensor is called to start
+     * @param intent is the intent of the service
+     * @param flags the flag of the service
+     * @param startID the start identification
+     * @return an integer that schedules if the service can be killed
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startID)
     {
-        this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        this.accelerometer = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        this.sensorManager.registerListener(this, this.accelerometer, SensorManager.SENSOR_DELAY_UI);
+        this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);      // Sets up the sensor service
+        this.accelerometer = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);        // Sets up the type of sensor
+        this.sensorManager.registerListener(this, this.accelerometer, SensorManager.SENSOR_DELAY_UI);       // Sets up the listener rate
 
-        return START_STICKY;
+        return START_STICKY;        // Returns an integer for the service schedule
     }
 
+    /**
+     * This method is called every time the sensor detects a change or gets an update information
+     * @param event is the event that happened to the sensor
+     */
     @Override
     public void onSensorChanged(SensorEvent event)
     {
-        this.currentCount++;
-        this.accelerometer_data = new double[3];
+        this.currentCount++;        // Increments the current count variable
+        this.accelerometer_data = new double[3];        // Resets the double variable to be new
 
-        accelerometer_data[0] = event.values[0];     // Accelerometer value with gravity on the x-axis
-        accelerometer_data[1] = event.values[1];     // Accelerometer value with gravity on the y-axis
-        accelerometer_data[2] = event.values[2];     // Accelerometer value with gravity on the z-axis
+        this.accelerometer_data[0] = event.values[0];     // Accelerometer value with gravity on the x-axis
+        this.accelerometer_data[1] = event.values[1];     // Accelerometer value with gravity on the y-axis
+        this.accelerometer_data[2] = event.values[2];     // Accelerometer value with gravity on the z-axis
 
-        String linearx = decimalFormat.format(accelerometer_data[0]);     // Limits the length of the x-axis value to 4 digits
-        String lineary = decimalFormat.format(accelerometer_data[1]);     // Limits the length of the y-axis value to 4 digits
-        String linearz = decimalFormat.format(accelerometer_data[2]);     // Limits the length of the z-axis value to 4 digits
+        this.linearx = this.decimalFormat.format(this.accelerometer_data[0]);     // Limits the length of the x-axis value to 4 digits
+        this.lineary = this.decimalFormat.format(this.accelerometer_data[1]);     // Limits the length of the y-axis value to 4 digits
+        this.linearz = this.decimalFormat.format(this.accelerometer_data[2]);     // Limits the length of the z-axis value to 4 digits
 
         final String accelerometerValues =      // Shows the values in a string.
                 this.systemInformation.getDateTime("yyyy/MM/dd HH:mm:ss:SSS") + "," +          // Starts a new string line.
-                        linearx + "," +         // Acceleration value on x-axis
-                        lineary + "," +         // Acceleration value on y-axis
-                        linearz;        // Acceleration value on z-axis
+                        this.linearx + "," +         // Acceleration value on x-axis
+                        this.lineary + "," +         // Acceleration value on y-axis
+                        this.linearz;        // Acceleration value on z-axis
 
         stringBuilder1.append(accelerometerValues);      // Appends the values to the string builder
         stringBuilder1.append("\n");     // Makes a new line
 
-        if ((this.stringBuilder1 != null) && (this.currentCount >= Integer.valueOf(Objects.requireNonNull(this.sharedPreferences.getString("accelerometer_data_limit", "")))))
+        if ((this.stringBuilder1 != null) && (this.currentCount >= Integer.valueOf(Objects.requireNonNull(this.sharedPreferences.getString("accelerometer_data_limit", "")))))      // Checks if the conditions are met
         {
-            this.stringBuilder2.append(this.stringBuilder1);
-            this.stringBuilder1.setLength(0);
-            this.currentCount = 0;
+            this.stringBuilder2.append(this.stringBuilder1);        // Appends all the values of the string builder to another
+            this.stringBuilder1.setLength(0);       // Resets the string builder
+            this.currentCount = 0;      // Resets the variable
 
-            new Thread(new Runnable()
+            new Thread(new Runnable()       // Sets a runnable thread
             {
+                /**
+                 * This method is called to run once the conditions are met
+                 */
                 @Override
                 public void run()
                 {
-                    dataLogger = new DataLogger(getApplicationContext(), getResources().getString(R.string.subdirectory_accelerometer), getResources().getString(R.string.accelerometer), stringBuilder2.toString());
-                    dataLogger.saveData("log");
+                    dataLogger = new DataLogger(getApplicationContext(), getResources().getString(R.string.subdirectory_accelerometer), getResources().getString(R.string.accelerometer), stringBuilder2.toString());       // Sets up the items to be logged
+                    dataLogger.saveData("log");     // Saves the data in the format specified
                 }
-            }).start();
+            }).start();     // Starts the runnable
         }
     }
 
+    /**
+     * This method is called if the class is to be killed for some reason
+     */
     @Override
     public void onDestroy()
     {
-        this.sensorManager.unregisterListener(this);
+        this.sensorManager.unregisterListener(this);        // Unregisters the sensor change listener
     }
 
+    /**
+     * This method is used to set the accuracy of the sensor
+     * @param sensor is the sensor to be used
+     * @param accuracy is the accuracy wanted
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy)
     {
         // Please do not remove this, the code needs this to function properly. Thank you :-)
     }
 
+    /**
+     * Unknown method implementation needed to run
+     * @param intent is the intent of the service
+     * @return unknown.
+     */
     @Override
     public IBinder onBind(Intent intent)
     {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        throw new UnsupportedOperationException("Not yet implemented");     // Message about an error
     }
 }
