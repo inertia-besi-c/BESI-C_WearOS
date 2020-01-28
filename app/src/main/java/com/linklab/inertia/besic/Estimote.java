821715dfb912fae0a344a5efe1af3b030f3146ee
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-01-28 15:45:10 -0500

    Commented class

--- app/src/main/java/com/linklab/inertia/besic/Estimote.java
+++ app/src/main/java/com/linklab/inertia/besic/Estimote.java
@@ -1,5 +1,8 @@
 package com.linklab.inertia.besic;
 
+/*
+ * Imports needed by the system to function appropriately
+ */
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -13,6 +16,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
+/**
+ * This class is responsible for the data getting from the bluetooth sensor and beacons set up. This class is started by the
+ * Sensor Timer superclass and kills itself after the specified time period.
+ */
 public class Estimote extends SensorTimer
 {
     private SharedPreferences sharedPreferences;        // Initializes the preference item
@@ -57,33 +64,36 @@ public class Estimote extends SensorTimer
             }
         }, Integer.valueOf(Objects.requireNonNull(this.sharedPreferences.getString("estimote_duration", ""))) * 1000);      // Repeats at the specified interval
 
-        startRanging();
+        startRanging();     // Calls the method to start running
 
-        return START_STICKY;
+        return START_STICKY;        // Makes sure the method can run outside of the application context
     }
 
+    /**
+     * This method starts looking for the beacons system set up and logs the data to the specified file
+     */
     public void startRanging()
     {
-        this.beaconManager = new BeaconManager(getApplicationContext());
-        this.beaconRegion = new BeaconRegion("ranged region", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+        this.beaconManager = new BeaconManager(getApplicationContext());        // Sets up a new beacon with the application context
+        this.beaconRegion = new BeaconRegion("ranged region", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);     // Implements the region needed to be looking for
 
-        this.beaconManager.connect(new BeaconManager.ServiceReadyCallback()
+        this.beaconManager.connect(new BeaconManager.ServiceReadyCallback()     // Connects to the region
         {
             @Override
-            public void onServiceReady()
+            public void onServiceReady()        // Checks if the service is ready
             {
-                beaconManager.startRanging(beaconRegion);
+                beaconManager.startRanging(beaconRegion);       // Calls the system to start looking for the region
             }
         });
 
-        this.beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener()
+        this.beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener()     // Sets the listener for the range
         {
             @Override
-            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<com.estimote.coresdk.recognition.packets.Beacon> beacons)
+            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<com.estimote.coresdk.recognition.packets.Beacon> beacons)       // This is called if a beacon is discovered in the region
             {
-                if (!beacons.isEmpty())
+                if (!beacons.isEmpty())     // Makes sure that the list being checked is not empty
                 {
-                    for (com.estimote.coresdk.recognition.packets.Beacon beacon : beacons)
+                    for (com.estimote.coresdk.recognition.packets.Beacon beacon : beacons)      // For every beacon in the list
                     {
                         data = systemInformation.getDateTime("yyyy/MM/dd HH:mm:ss:SSS") + (",") + "Estimote Service" + (",") + beacon.getMajor() + (",") + beacon.getMinor() + (",") + beacon.getRssi() + (",") + beacon.getMacAddress() + (",") + ("\n");       // Data to be logged by the system
                         dataLogger = new DataLogger(getApplicationContext(), getResources().getString(R.string.subdirectory_sensors), getResources().getString(R.string.estimote), data);      // Sets a new datalogger variable
@@ -94,10 +104,16 @@ public class Estimote extends SensorTimer
         });
     }
 
+    /**
+     * The destructor for the method
+     */
     @Override
     public void onDestroy()
     {
-        this.beaconManager.stopRanging(beaconRegion);
-        this.stopForeground(true);
+        this.beaconManager.stopRanging(beaconRegion);       // Calls the beacon range to stop
+        this.timer.cancel();        // Cancels the timer made by the class
+        this.stopForeground(true);      // Stops the foreground notification
+
+        super.onDestroy();      // Calls the superclass on destroy method
     }
 }
